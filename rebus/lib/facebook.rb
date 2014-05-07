84b599723aebba80096dabcbeb3ee7af665b582b
Crustacean87! <declanv@gmail.com>
2014-05-07 15:56:51 -0400

    Adding saved changes

--- rebus/lib/facebook.rb
+++ rebus/lib/facebook.rb
@@ -11,3 +11,13 @@ class Instagram
   end
 
 end
+
+ def get_photo(word)
+    url = "https://api.instagram.com/v1/tags/#{word}/media/recent?access_token=51067521.f59def8.6ef9f6339ddc413b953086f6def34593"
+    response = HTTParty.get(url)
+
+    image_url = response["data"][0]["images"]["standard_resolution"]["url"]
+
+    image_url
+
+  end
