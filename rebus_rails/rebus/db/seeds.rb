3f651ebd3ddfe01db4fa792e23de2e993ac03689
Crustacean87! <declanv@gmail.com>
2014-05-15 17:48:50 -0400

    Made styling changes to fix media queries.

--- rebus_rails/rebus/db/seeds.rb
+++ rebus_rails/rebus/db/seeds.rb
@@ -1,9 +1,9 @@
 Post.delete_all
 
 Post.create({
-    input_1: "http://fc04.deviantart.net/fs10/f/2006/325/f/2/Square_Animals___Muhkuh_by_dogta.jpg",
+    input_1: "beyonce",
     input_2: "ate",
-    input_3: "http://www.spiralgraphics.biz/packs/path/previews/Open%20Square%20Path%20Grass.jpg",
+    input_3: "kanye",
     input_4: "in",
-    input_5: "http://johnwclarke.files.wordpress.com/2010/10/times-square-15-seconds.jpg"
+    input_5: "cholera"
     })
